3b71e4deeafe90e93770a881ce8c283a01e68761
Enrico Rossi <e.rossi@tecnobrain.com>
2010-10-13 14:49:21 +0200

    initial import from the chipcard project

---
+++ src/sle.c
@@ -0,0 +1,187 @@
+/* This file is part of libsle4442.
+ * Copyright (C) 2010 Enrico Rossi
+ *
+ * Libsle4442 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Libsle4442 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <avr/io.h>
+#include "sle_bbg.h"
+#include "sle.h"
+
+struct sle_t* sle_init(void)
+{
+	struct sle_t *sle;
+
+	/*
+	   Inital PORT setup:
+	   Card present 1 Pulled UP by micro
+	   Reset 0
+	   Clock 0
+	   IO 1 Pulled UP when Reading
+	 */
+
+	SLE_PORT = _BV(SLE_PRESENT) | _BV(SLE_IO);
+
+	/*
+	   Initial DDR setup:
+	   Card Present - IN
+	   Reset - OUT
+	   Clock - OUT
+	   IO - BiDirectional begin IN
+	 */
+
+	SLE_DDR = _BV(SLE_RST) | _BV(SLE_CK);
+
+	sle = malloc(sizeof(struct sle_t));
+	sle->atr = malloc(4);
+	sle->main_memory = malloc(256);
+	sle->protected_memory = malloc(4);
+	sle->security_memory = malloc(4);
+	/* Normally we should use only 1, but during the auth
+	   it's wise to keep all the 5 processing result */
+	sle->ck_proc = malloc(5);
+	return(sle);
+}
+
+void sle_free(struct sle_t *sle)
+{
+	free(sle->ck_proc);
+	free(sle->security_memory);
+	free(sle->protected_memory);
+	free(sle->main_memory);
+	free(sle->atr);
+	free(sle);
+}
+
+void sle_reset(uint8_t *atr)
+{
+	send_rst(atr);
+}
+
+/*! \brief Check for the presence of the card. */
+uint8_t sle_present(struct sle_t *sle)
+{
+
+	if (SLE_PIN & _BV(SLE_PRESENT))
+		sle->card_present=1;
+	else
+		sle->card_present=0;
+
+	return(sle->card_present);
+}
+
+void sle_dump_memory(uint8_t *mm)
+{
+	int i;
+
+	send_cmd(SLE_CMD_DUMP_MEMORY, 0, 0);
+
+	for (i=0; i<256; i++)
+		*(mm+i) = read_byte();
+
+	ck_pulse(); /* leave the card to high imp. I/O line */
+}
+
+void sle_dump_prt_memory(uint8_t *mm)
+{
+	uint8_t i;
+
+	send_cmd(SLE_CMD_DUMP_PRT_MEMORY, 0, 0);
+
+	for (i=0; i<4; i++)
+		*(mm+i) = read_byte();
+
+	ck_pulse(); /* leave the card to high imp. I/O line */
+}
+
+void sle_dump_secmem(uint8_t *mm)
+{
+	uint8_t i;
+
+	send_cmd(SLE_CMD_DUMP_SECMEM, 0, 0);
+
+	for (i=0; i<4; i++)
+		*(mm+i) = read_byte();
+
+	ck_pulse(); /* leave the card to high imp. I/O line */
+}
+
+void sle_dump_allmem(struct sle_t *sle)
+{
+	sle_dump_memory(sle->main_memory);
+	sle_dump_prt_memory(sle->protected_memory);
+	sle_dump_secmem(sle->security_memory);
+}
+
+void sle_auth(struct sle_t *sle, const uint8_t pin1, const uint8_t pin2, const uint8_t pin3)
+{
+	/* dump secmem */
+	sle_dump_secmem(sle->security_memory);
+
+	/* Check error = 7, all available error bit = 1 */
+	if (*(sle->security_memory) == 7) {
+
+		/* write 0 to bit 3 */
+		send_cmd(SLE_CMD_UPDATE_SECMEM, 0, 3);
+		*(sle->ck_proc) = processing();
+
+		/* Compare 3 byte PIN */
+		send_cmd(SLE_CMD_COMPARE_VERIFICATION_DATA, 1, pin1);
+		*(sle->ck_proc + 1) = processing();
+		send_cmd(SLE_CMD_COMPARE_VERIFICATION_DATA, 2, pin2);
+		*(sle->ck_proc + 2) = processing();
+		send_cmd(SLE_CMD_COMPARE_VERIFICATION_DATA, 3, pin3);
+		*(sle->ck_proc + 3) = processing();
+
+		/* write 0xff to error */
+		send_cmd(SLE_CMD_UPDATE_SECMEM, 0, 0xff);
+		*(sle->ck_proc + 4) = processing();
+
+		/* redump secmem */
+		sle_dump_secmem(sle->security_memory);
+	}
+
+	/* if error = 7 then auth is OK */
+	if (*(sle->security_memory) == 7)
+		sle->auth = 1;
+}
+
+void sle_write_memory(struct sle_t *sle, const uint8_t base, const uint8_t len)
+{
+	uint8_t i, addr;
+
+	if (sle->auth) {
+		for (i=0; i<len; i++) {
+			addr = base + i;
+			send_cmd(SLE_CMD_UPDATE_MEMORY, addr, *(sle->main_memory + addr));
+			*(sle->ck_proc) = processing();
+		}
+	}
+}
+
+void sle_write_secmem(struct sle_t *sle)
+{
+	uint8_t i;
+
+	if (sle->auth) {
+		for (i=0; i<4; i++) {
+			send_cmd(SLE_CMD_UPDATE_SECMEM, i, *(sle->security_memory + i));
+			*(sle->ck_proc + i) = processing();
+		}
+	}
+}
+
---
+++ src/sle.h
@@ -0,0 +1,102 @@
+/* This file is part of libsle4442.
+ * Copyright (C) 2010 Enrico Rossi
+ *
+ * Libsle4442 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Libsle4442 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*!
+  \file sle.h
+  \brief This is the API avariable to access to SLE4442.
+
+  Details.
+ */
+
+#ifndef SLE_H_
+#define SLE_H_
+
+/*!
+  \def SLE_CMD_DUMP_MEMORY
+  \brief SLE4442 Hex command to access the main memory.
+
+  \def SLE_CMD_DUMP_SECMEM
+  \brief SLE4442 Hex command to access the security memory.
+
+  \def SLE_CMD_DUMP_PRT_MEMORY
+  \brief SLE4442 Hex command to access the protected memory.
+ */
+
+#define SLE_CMD_DUMP_MEMORY 0x30
+#define SLE_CMD_DUMP_SECMEM 0x31
+#define SLE_CMD_DUMP_PRT_MEMORY 0x34
+#define SLE_CMD_COMPARE_VERIFICATION_DATA 0x33
+#define SLE_CMD_UPDATE_SECMEM 0x39
+#define SLE_CMD_UPDATE_MEMORY 0x38
+
+/*!
+  \struct sle_t sle.h "sle.h"
+  \brief The main struct which represent the status of the card.
+
+  Keep in mind AtMega chips have only 1 KB RAM, dumping all the
+  card memory into RAM may cause an out of memory.
+
+  \var uint8_t *sle_t::atr
+  \brief ptr to a 4 bytes ATR header returned by the ATZ command.
+
+  \var uint8_t *sle_t::main_memory
+  \brief ptr to a 256 bytes copy of the SLE 4442 memory.
+
+  \var uint8_t *sle_t::protected_memory
+  \brief ptr to a 32 bytes copy of the SLE 4442 memory.
+
+  \var uint8_t* sle_t::security_memory
+  \brief 4 bytes
+
+  \var uint8_t* sle_t::ck_proc
+  \brief processing clock counts
+
+  \var uint8_t sle_t::card_present
+  \brief card present 0=no; 1=yes
+
+  \var uint8_t sle_t::auth
+  \brief card auth 1 - ok, 0 - writing forbidden
+
+  auth ok, I can write
+
+ */
+
+struct sle_t {
+	uint8_t *atr;
+	uint8_t *main_memory;
+	uint8_t *protected_memory;
+	uint8_t *security_memory;
+	uint8_t *ck_proc;
+	uint8_t card_present;
+	uint8_t auth;
+};
+
+struct sle_t* sle_init(void);
+void sle_free(struct sle_t *sle);
+void sle_reset(uint8_t *atr);
+uint8_t sle_present(struct sle_t *sle);
+void sle_dump_memory(uint8_t *mm);
+void sle_dump_prt_memory(uint8_t *mm);
+void sle_dump_secmem(uint8_t *mm);
+void sle_dump_allmem(struct sle_t *sle);
+void sle_auth(struct sle_t *sle, const uint8_t pin1, const uint8_t pin2, const uint8_t pin3);
+void sle_write_memory(struct sle_t *sle, const uint8_t addr, const uint8_t len);
+void sle_write_secmem(struct sle_t *sle);
+
+#endif
+
